a70750ff39b65ef207f1eba8bb7cec0db3fe6ccd
lakshmisubramaniam54 <47235121+lakshmisubramaniam54@users.noreply.github.com>
2019-02-15 19:22:43 +0530

    Create begq3.py

---
+++ begq3.py
@@ -0,0 +1,10 @@
+ch=input()
+if ch.isalpha():
+  if ch=='a' or ch =='e' or ch  =='i'or ch=='o'or ch=='u':
+      print("Vowel")
+  elif ch=='A' or ch=='E'or ch=='I' or ch=='O' or ch=='U':
+      print("Vowel")
+  else:
+      print("Consonant")
+else:
+`   print("Invalid")
