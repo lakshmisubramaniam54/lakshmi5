ec3c25d2191dde63be6d54cb62fac51871dcd5cc
lakshmisubramaniam54 <47235121+lakshmisubramaniam54@users.noreply.github.com>
2019-02-16 19:09:06 +0530

    Create begq72.py

---
+++ begq72.py
@@ -0,0 +1,11 @@
+ch=input()
+if ch.isalpha():
+    if ch=='a' or ch=='e' or ch=='i' or ch=='o' or ch=='u':
+        print("yes")
+    elif ch=='A'or ch=='E' or ch=='I' or ch=='O' or ch=='U':
+        print("yes")
+    else:
+        print("no")
+else:
+    print("no")
+    
