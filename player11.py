f0279597d4c5887663a33c6327ccaed3ed0ad3a6
lakshmisubramaniam54 <47235121+lakshmisubramaniam54@users.noreply.github.com>
2019-02-28 21:10:26 +0530

    Update player11.py

--- player11.py
+++ player11.py
@@ -1,5 +1,5 @@
 a=input()
-if(a=="Sunday"):
+if(a=="Sunday" or a=="Saturday"):
   print("yes")
-elif(a=="Monday" or a=="Tuesday" or a=="Wednesday" or a=="Thursday" or a=="Friday" or a=="Saturday"):
+elif(a=="Monday" or a=="Tuesday" or a=="Wednesday" or a=="Thursday" or a=="Friday" ):
   print("no")
