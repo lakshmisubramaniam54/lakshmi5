024b5b37442dc9f90be682d0e0614a77cafb3534
lakshmisubramaniam54 <47235121+lakshmisubramaniam54@users.noreply.github.com>
2019-03-02 16:57:30 +0530

    Create player22.c

---
+++ player22.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(void)
+  {
+      int a,b,c,i;
+      scanf("%d %d", &a,&b);
+      for(i=2;i<=n;i++)
+      {
+          if(b%i==0 && a%i==0)
+          {
+              c=i;
+          }
+      }
+      printf("%d"c);
+      return 0;
+  }
