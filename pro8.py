b330e3af0e1e609c5c907ba59c249ab2dd9645a8
lakshmisubramaniam54 <47235121+lakshmisubramaniam54@users.noreply.github.com>
2019-03-06 13:16:40 +0530

    Create pro8.py

---
+++ pro8.py
@@ -0,0 +1,15 @@
+def gcd(x,y):
+    if(y==0):
+        return x
+    else:
+        return gcd(y,x%y)
+n,q=map(int(input()).split())
+a=list(map(int,input().split()))
+w=[]
+while q!=0:
+    x,y=map(int,input().split())
+    z=gcd(a[x-1],a[y-1])
+    w.append(z)
+    q=q-1
+for i in w:
+    print(i)
