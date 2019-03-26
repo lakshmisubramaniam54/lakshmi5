4231b546fd5fb3993ad831345932062e94753d06
lakshmisubramaniam54 <47235121+lakshmisubramaniam54@users.noreply.github.com>
2019-03-26 09:32:20 +0530

    Update p32.py

--- p32.py
+++ p32.py
@@ -9,7 +9,7 @@ for i in range(0,d):
 c=sorted(c)
 for i in range(0,len(c[0])):
     for j in range(0,len(c)-1):
-        if(c[j][i]>c[j+1][i]):
+        if c[j][i]>c[j+1][i]:
         c[j][i],c[j+1][i]=c[j+1][i],c[j][i]
 for i in c:
     print(*i)
