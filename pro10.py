eaa1ea6e2cd1f2f894d005e480966797b9b13b8a
lakshmisubramaniam54 <47235121+lakshmisubramaniam54@users.noreply.github.com>
2019-05-03 14:44:58 +0530

    Create pro10.py

---
+++ pro10.py
@@ -0,0 +1,9 @@
+#lakshmi
+num=int(input())
+stepss=list(map(int,input().split()))
+tota=0
+for i in range(1,len(stepss)):
+	     for j in range(0,i):  
+		           if(stepss[j]<stepss[i]):
+			                 tota+=stepss[j]
+print(tota)
