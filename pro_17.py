1277f328564b5b1e5ad6f844255dcee21d5e943e
lakshmisubramaniam54 <47235121+lakshmisubramaniam54@users.noreply.github.com>
2019-06-17 23:16:31 +0530

    Create pro_17.py

---
+++ pro_17.py
@@ -0,0 +1,12 @@
+manp,sanp=map(int,input().split())
+arrayp=list(map(int,input().split()))
+ranp=0
+for i in range(len(arrayp)):
+	for j in range(i+1,len(arrayp)):
+		if (arrayp[i]+arrayp[j]==sanp):
+			ranp+=1
+			break
+if(ranp):
+	print("yes")
+else:
+	print("no")
